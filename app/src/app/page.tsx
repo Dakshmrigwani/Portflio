096e5680742fc53a967b1894c07a379981bd9b90
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-03-22 21:33:11 +0530

    Update page.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,7 +2,7 @@ import About from "./about-section";
 import { Experience } from "./experience";
 import { HeroHighlightDemo } from "./hero-section";
 import Marketing from "./marketing";
-import Projects from "./projects";
+// import Projects from "./projects";
 import Skills from "./skill";
 
 export default function Home() {
